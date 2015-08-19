23f88a5c1e0ac044eaea7a2f8ceb703ba18bc070
Ian Truslove <ian.truslove@gmail.com>
2015-08-19 08:03:40 -0600

    Simplifying and clarifying

--- src/static/core.clj
+++ src/static/core.clj
@@ -372,49 +372,43 @@
                         (catch Exception e
                           (log/error (str "Exception thrown while building site! " e))))))))
 
-(defn do-watch! [tmp?]
-  (let [out-dir (:out-dir (config/config))
-        tmp-dir (str (System/getProperty "java.io.tmpdir") "/" "static/")]
-    (when tmp?
-      (let [loc (FilenameUtils/normalize tmp-dir)]
-        (config/set!-config :out-dir loc)
-        (log/info (str "Using tmp location: " (:out-dir (config/config))))))
-    (do (watch-and-rebuild)
-        (future (jetty/run-jetty serve-static {:port 8080}))
-        (browse/browse-url "http://127.0.0.1:8080"))))
+(defn new-tmp-dir []
+  (str (System/getProperty "java.io.tmpdir") "/" "static/"))
+
+(defn do-watch! [use-system-tmp-dir?]
+  (when use-system-tmp-dir?
+    (config/set!-config :out-dir (FilenameUtils/normalize (new-tmp-dir)))
+    (log/info (str "Using tmp location: " (:out-dir (config/config)))))
+  (watch-and-rebuild)
+  (future (jetty/run-jetty serve-static {:port 8080}))
+  (browse/browse-url "http://127.0.0.1:8080"))
 
 (defn do-build! []
-  (let [out-dir (:out-dir (config/config))
-        tmp-dir (str (System/getProperty "java.io.tmpdir") "/" "static/")]
-    (when (:atomic-build (config/config))
-      (let [loc (FilenameUtils/normalize tmp-dir)]
-        (config/set!-config :out-dir loc)
-        (log/info (str "Using tmp location: " (:out-dir (config/config))))))
+  (let [tmp-dir (new-tmp-dir)
+        atomic-build? (:atomic-build (config/config))]
+    (when atomic-build?
+      (config/set!-config :out-dir (FilenameUtils/normalize tmp-dir))
+      (log/info (str "Using tmp location: " (:out-dir (config/config)))))
     (logging/log-time-elapsed "Build took "
                               (create))
-    (when (:atomic-build (config/config))
-      (FileUtils/deleteDirectory (File. out-dir))
-      (FileUtils/moveDirectory (File. tmp-dir) (File. out-dir)))
+    (when atomic-build?
+      (let [out-dir (:out-dir (config/config))]
+        (FileUtils/deleteDirectory (File. out-dir))
+        (FileUtils/moveDirectory (File. tmp-dir) (File. out-dir))))
     (shutdown-agents)))
 
-(defn do-jetty! [tmp?]
-  (let [out-dir (:out-dir (config/config))
-        tmp-dir (str (System/getProperty "java.io.tmpdir") "/" "static/")]
-    (when tmp?
-      (let [loc (FilenameUtils/normalize tmp-dir)]
-        (config/set!-config :out-dir loc)
-        (log/info (str "Using tmp location: " (:out-dir (config/config))))))
-    (do (future (jetty/run-jetty serve-static {:port 8080}))
-        (browse/browse-url "http://127.0.0.1:8080"))
-    (shutdown-agents)))
+(defn do-jetty! [use-system-tmp-dir?]
+  (when use-system-tmp-dir?
+    (config/set!-config :out-dir (FilenameUtils/normalize (new-tmp-dir)))
+    (log/info (str "Using tmp location: " (:out-dir (config/config)))))
+  (future (jetty/run-jetty serve-static {:port 8080}))
+  (browse/browse-url "http://127.0.0.1:8080")
+  (shutdown-agents))
 
-(defn do-rsync! [tmp?]
-  (let [out-dir (:out-dir (config/config))
-        tmp-dir (str (System/getProperty "java.io.tmpdir") "/" "static/")]
-    (when tmp?
-      (let [loc (FilenameUtils/normalize tmp-dir)]
-        (config/set!-config :out-dir loc)
-        (log/info (str "Using tmp location: " (:out-dir (config/config))))))
-    (let [{:keys [rsync out-dir host user deploy-dir]} (config/config)]
-      (io/deploy-rsync rsync out-dir host user deploy-dir))
-    (shutdown-agents)))
+(defn do-rsync! [use-system-tmp-dir?]
+  (when use-system-tmp-dir?
+    (config/set!-config :out-dir (FilenameUtils/normalize (new-tmp-dir)))
+    (log/info (str "Using tmp location: " (:out-dir (config/config)))))
+  (let [{:keys [rsync out-dir host user deploy-dir]} (config/config)]
+    (io/deploy-rsync rsync out-dir host user deploy-dir))
+  (shutdown-agents))
