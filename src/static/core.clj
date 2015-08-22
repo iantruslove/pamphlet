887ea5bda2a7514f2ad1ae13d654ba93622180bc
Ian Truslove <ian.truslove@gmail.com>
2015-08-22 16:28:45 -0600

    Build the site before starting up the webserver
    Conflicts:
    	src/static/core.clj

--- src/static/core.clj
+++ src/static/core.clj
@@ -327,7 +327,9 @@
   (let [{create-archives? :create-archives
          blog-as-index? :blog-as-index
          :keys [out-dir]} (config/config)]
+    (log/info "Rebuilding site...")
     (doto (File. out-dir)
+
       (FileUtils/deleteDirectory)
       (.mkdir))
 
@@ -355,6 +357,15 @@
            (File. (str out-dir "latest-posts/" max "/index.html"))
            (File. (str out-dir "index.html"))))))))
 
+(defn create-quietly
+  "Build site, logging and swallowing exceptions."
+  []
+  (try
+    (memo-clear! io/read-template)
+    (create)
+    (catch Exception e
+      (log/error e "Exception thrown while building site!"))))
+
 (defn serve-static [req]
   (let [mime-types {".clj" "text/plain"
                     ".mp4" "video/mp4"
@@ -371,12 +382,7 @@
                    (watcher/rate 1000)
                    (watcher/on-change
                     (fn [_]
-                      (log/info "Rebuilding site...")
-                      (try
-                        (memo-clear! io/read-template)
-                        (create)
-                        (catch Exception e
-                          (log/error (str "Exception thrown while building site! " e))))))))
+                      (create-quietly)))))
 
 (defn new-tmp-dir []
   (str (System/getProperty "java.io.tmpdir") "static/"))
@@ -385,8 +391,9 @@
   (when use-system-tmp-dir?
     (config/set-config! :out-dir (FilenameUtils/normalize (new-tmp-dir)))
     (log/info (str "Using tmp location: " (config/config :out-dir))))
-  (watch-and-rebuild)
+  (create-quietly)
   (future (jetty/run-jetty serve-static {:port 8080}))
+  (watch-and-rebuild)
   (browse/browse-url "http://127.0.0.1:8080"))
 
 (defn do-build! []
