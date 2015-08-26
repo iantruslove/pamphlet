fd802b9df4300d67a7f67a0b5a215df45b21770e
Ian Truslove <ian.truslove@gmail.com>
2015-08-26 08:12:10 -0600

    Altered cache partitioning to allow per-file invalidation
    Conflicts:
    	src/static/io.clj

--- src/static/io/cache.clj
+++ src/static/io/cache.clj
@@ -7,20 +7,38 @@
 
 (defonce file-cache (atom (new-file-cache)))
 
+;; A map of files to sets of partitions, used for cache
+;; invalidation. An inverted index of sorts.
+(defonce cache-partitions-to-files (atom {}))
+
+(defn associate [partitions file partition]
+  (update partitions file (fn [part-set]
+                            (conj (or part-set #{}) partition))))
+
+(defn cache-key [file f-miss]
+  [file f-miss])
+
 (defn read-cached-file
   "Tries to read filename from the cache. If not in cache, f-miss will
   be called with filename as the first argument, the results of which
   will be added to the cache."
-  [[partition filename :as cache-key] f-miss]
-  (-> (if (cache/has? @file-cache cache-key)
-        (do
-          (log/trace "Cache hit:" cache-key)
-          (cache/hit @file-cache cache-key))
-        (do
-          (log/trace "Cache miss:" cache-key)
-          (swap! file-cache cache/miss cache-key (f-miss filename))))
-      (get cache-key)))
+  [file f-miss]
+  (let [cache-key (cache-key file f-miss)]
+    (-> (if (cache/has? @file-cache cache-key)
+          (do
+            (log/trace "Cache hit:" cache-key)
+            (cache/hit @file-cache cache-key))
+          (do
+            (log/trace "Cache miss:" cache-key)
+            ;; Use the processing function as the partition value:
+            (swap! cache-partitions-to-files associate file f-miss)
+            (swap! file-cache cache/miss cache-key (f-miss file))))
+        (get cache-key))))
 
-(defn invalidate-cache [cache-key]
-  (log/debug "Invalidating cache:" cache-key)
-  (swap! file-cache cache/evict cache-key))
+(defn invalidate-cache [file]
+  "Invalidates all cache entries for `file`, regardless of the
+  partition they are in."
+  (doseq [partition (get @cache-partitions-to-files file)
+          :let [cache-key (cache-key file partition)]]
+    (log/debug "Invalidating cache:" cache-key)
+    (swap! file-cache cache/evict cache-key)))
